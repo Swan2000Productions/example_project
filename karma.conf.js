dc07ae6b0e384fa22089d25f2c2e47bf1227a329
unknown <robertpelyhe2000@gmail.com>
2025-01-22 23:15:54 +0100

    modified karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,44 +1,44 @@
-module.exports = function(config) {
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
   config.set({
-    // Base path, that will be used to resolve all patterns (like files, exclude)
     basePath: '',
-
-    // Frameworks to use (Jasmine is default for Angular projects)
-    frameworks: ['jasmine'],
-
-    // List of files/patterns to load in the browser
-    files: [
-      'src/test.ts', // The main entry point for testing
-      { pattern: 'src/**/*.spec.ts', watched: false } // Include all .spec.ts files
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-
-    // Preprocessors to use (none for this example)
-    preprocessors: {
-      'src/test.ts': ['karma-typescript'], // Add TypeScript support if needed
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-
-    // Reporters to use (default is 'progress')
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/example_project'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
     reporters: ['progress', 'kjhtml'],
-
-    // Port for the Karma server
     port: 9876,
-
-    // Enable/disable colors in the output (useful for debugging)
     colors: true,
-
-    // Level of logging (karma-verbose for more details)
     logLevel: config.LOG_INFO,
-
-    // Enable/disable watching files and executing tests whenever anything changes
     autoWatch: true,
-
-    // Start these browsers (use Chrome for local testing)
     browsers: ['Chrome'],
-
-    // Continuous Integration mode (if true, Karma captures browsers, runs the tests, and exits)
     singleRun: false,
-
-    // Concurrency level (how many browsers to start at once)
-    concurrency: Infinity
+    restartOnFileChange: true
   });
 };
